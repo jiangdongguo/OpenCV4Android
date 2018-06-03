2a015710cce647b6fead4ba4b76470efc2d4ac28
jiangdongguo <765067602@qq.com>
2018-06-03 22:07:37 +0800

    修复cv::Mat引用错误

--- openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: E:\\StudioProject\\OpenCV4Android\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: E:\\Android\\StudioProject\\OpenCV4Android\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
